61ac0853c8e510aeee64e4fe697e02f3e0769059
Iskander Foatov <izzy88man@gmail.com>
2019-10-20 20:38:12 +0300

    initail commit: XCode SPA template

---
+++ Tau/Tau/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tau
+//
+//  Created by Iskander Foatov on 20.10.2019.
+//  Copyright © 2019 Iskander Foatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
